d46ebe44a06fba82cf9c703c78dc68659dfa7ea4
tijsstraatman <tijsstraatman@gmail.com>
2017-09-13 11:48:20 +0200

    Merged changes

--- src/main/java/TripManager.java
+++ src/main/java/TripManager.java
@@ -1,21 +1,18 @@
-package main.java;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TripManager {
 
-    int tripnumber = 0;
-    LocalDateTime endtime;
-    LocalDate enddate;
+    private List<BoatTrip> trips = new ArrayList<BoatTrip>();
 
-    public TripManager(int tripnumber, LocalDateTime endtime, LocalDate enddate) {
-        this.tripnumber = tripnumber;
-        this.endtime = endtime;
-        this.enddate = enddate;
+    public void createTrip() {
+        int identifier = trips.size() + 1;
+        BoatTrip boatTrip = new BoatTrip(LocalDateTime.now(), identifier);
+        trips.add(boatTrip);
     }
 
-//    public void endTrip(int tripnumber, List<BoatTrip> boatlist) {
+//    public static void endTrip(int tripnumber, List<Boat> boatlist) {
 //        for (int i = 0; i < boatlist.size(); i++) {
 //            if (tripnumber == getTripNumber) {
 //                // Op plaats Boattrip moet de verwijzing naar tripnumberclass komen
