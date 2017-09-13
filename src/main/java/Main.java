cad5183f281e583609ad2434194104d43a0a53c2
trozie <twanrozie@gmail.com>
2017-09-13 14:10:20 +0200

    Removed StartDate, added TripCounter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,28 @@
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-//        LocalDateTime currentTime = LocalDateTime.now();
-//        int identifier = 1;
-//        BoatTrip boatTrip1 = new BoatTrip(currentTime, identifier);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yyyy  HH:mm");
-//        String out = currentTime.format(formatter);
-//        System.out.println(out);
+        LocalDateTime currentTime = LocalDateTime.now();
+        int identifier = 1;
+        BoatTrip boatTrip1 = new BoatTrip(currentTime, identifier);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yyyy  HH:mm");
+        String out = currentTime.format(formatter);
+        System.out.println(out);
+        Thread.sleep(5000);
+        Duration d = Duration.between(currentTime.minusHours(1), LocalDateTime.now());
+        System.out.println("Time of the boattrip was " + d.getSeconds() + " seconds");
+
+        if (d.getSeconds() > 3600) {
+           double uren = Math.floor(d.getSeconds()/3600);
+           double minuten = Math.floor((d.getSeconds()%3600)/60);
+           System.out.println("Time of the boattrip was " + uren + " uur en " + minuten + " minuten");
+        }
+
 
         TripManager manager = new TripManager();
 
