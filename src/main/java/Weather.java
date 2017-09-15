4e0830d808237a39cd1a5d0307d84fc41f807463
trozie <twanrozie@gmail.com>
2017-09-15 10:03:43 +0200

    begin weer voor prijsberekening toegevoegd

---
+++ src/main/java/Weather.java
@@ -0,0 +1,14 @@
+public class Weather {
+
+    private boolean isRaining = true;
+
+    private int temprature = 19;
+
+    public boolean isRaining() {
+        return isRaining;
+    }
+
+    public int getTemprature() {
+        return temprature;
+    }
+}
