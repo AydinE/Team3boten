cbe24f864b1d138058f1c903d4763b0a65162f7e
Emielvr <emielvr@hotmail.com>
2017-09-15 11:06:34 +0200

    Caught exception in weather info

--- src/main/java/WeatherInfo.java
+++ src/main/java/WeatherInfo.java
@@ -11,30 +11,31 @@ public class WeatherInfo {
     private boolean raining;
     private BigDecimal temperature = new BigDecimal(0);
 
-    public void updateWeatherData() throws IOException {
-        // Dit is de API request met mijn persoonlijke API key (gratis voor < 60 requests per minuut)
-        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?id=6426072&APPID=7d0438aeeeb1d75e74a9825f9ae539c3&units=metric");
+    public void updateWeatherData() {
+        try {
+            // Dit is de API request met mijn persoonlijke API key (gratis voor < 60 requests per minuut)
+            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?id=6426072&APPID=7d0438aeeeb1d75e74a9825f9ae539c3&units=metric");
 
-        try (InputStream stream = url.openStream()) {
-            String output = convertStreamToString(stream);
+            try (InputStream stream = url.openStream()) {
+                String output = convertStreamToString(stream);
 
-            // Converteer teruggekeerde string naar JSON
-            JSONObject jsonObj = new JSONObject(output.toString());
-            JSONObject main = jsonObj.getJSONObject("main");
-            JSONArray weather = jsonObj.getJSONArray("weather");
-            int weatherId = (int) weather.getJSONObject(0).get("id");
+                // Converteer teruggekeerde string naar JSON
+                JSONObject jsonObj = new JSONObject(output.toString());
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONArray weather = jsonObj.getJSONArray("weather");
+                int weatherId = (int) weather.getJSONObject(0).get("id");
 
-            //temperature = (double) main.get("temp");
-            temperature = new BigDecimal(String.valueOf(main.get("temp")));
+                //temperature = (double) main.get("temp");
+                temperature = new BigDecimal(String.valueOf(main.get("temp")));
 
-            // De API geeft ID codes aan het weer, code 500 t/m 599 is gereserveerd voor weersituaties met regen
-            // Als het ID dus buiten 500 t/m 599 valt dan regent het niet.
-            if (weatherId >= 500 && weatherId <= 599) {
-                raining = true;
-            } else {
-                raining = false;
+                // De API geeft ID codes aan het weer, code 500 t/m 599 is gereserveerd voor weersituaties met regen
+                // Als het ID dus buiten 500 t/m 599 valt dan regent het niet.
+                if (weatherId >= 500 && weatherId <= 599) {
+                    raining = true;
+                } else {
+                    raining = false;
+                }
             }
-
         } catch (IOException e) {
             System.out.println("Geen weerinformatie beschikbaar, kijk naar buiten voor actuele weer informatie");
         }
